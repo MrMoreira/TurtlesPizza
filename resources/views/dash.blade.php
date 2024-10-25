66d05e86ccc24fac7ba487e0778dcd15da8c6c59
Caique Moreira <47729557+MrMoreira@users.noreply.github.com>
2024-10-24 21:51:44 -0300

    0.0.0.9
    Alteração no layout

--- resources/views/dash.blade.php
+++ resources/views/dash.blade.php
@@ -11,17 +11,43 @@
         @include('templates.navbar') <!-- Inclui a navbar -->
         
         <main class="main-p">
-        <div class="main-inbox">
-            <p>Para Os Amantes De Pizza</p>
-            <h1>A Melhor Pizza Da Cidade</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic veniam voluptas laborum, perspiciatis,
-                culpa deleniti exercitationem velit natus unde, necessitatibus repellat at architecto enim corporis eos
-                illum. Minima, ipsa officia.
-            </p>
-            <p>Clique Aqui</p>
-        </div>
-        
-    </main>
+            <div class="main-inbox">
+                <p>Para Os Amantes De Pizza</p>
+                <h1>A Melhor Pizza Da Cidade</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic veniam voluptas laborum, perspiciatis,
+                    culpa deleniti exercitationem velit natus unde, necessitatibus repellat at architecto enim corporis eos
+                    illum. Minima, ipsa officia.
+                </p>
+                <p>Clique Aqui</p>
+            </div>
+
+            <div class="category">
+                <!--    
+                Tipos de Pizza no menu categoria 
+                Salgadas, Doces, Bebidas, Porções, Sobremesas
+                -->
+                <div class="salgadas">
+                    <img src="/img/salgada.png" alt="Salgadas">
+                    <p>Salgadas</p>
+                </div>
+                <div class="doces">
+                    <img src="/img/doces.png" alt="Doces">
+                    <p>Doces</p>
+                </div>
+                <div class="bebidas">
+                    <img src="/img/bebidas.png" alt="Bebidas">
+                    <p>Bebidas</p>
+                </div>
+                <div class="porcoes">
+                    <img src="/img/porcoes.png" alt="Porções">
+                    <p>Porções</p>
+                </div>
+                <div class="sobremesas">
+                    <img src="/img/sobremesas.png" alt="Sobremesas">
+                    <p>Sobremesas</p>
+                </div>
+            </div>
+        </main>
 
         <!-- Inclui o footer -->
         @include('templates.footer') <!-- Inclui a navbar -->
